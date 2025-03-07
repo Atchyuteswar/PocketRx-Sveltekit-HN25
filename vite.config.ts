e02eb0640f786a00480b84e6f8910300046f6f73
Atchyuteswar <yt.atchyut@gmail.com>
2025-03-07 07:51:23 +0530

    fixing bugs with deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: ['@emailjs/browser']
+	},
+	build: {
+		rollupOptions: {
+			external: ['@emailjs/browser'],
+			output: {
+				globals: {
+					'@emailjs/browser': 'emailjs'
+				}
+			}
+		}
+	}
 });
